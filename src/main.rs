f4b2c4bdb99afa30db363774f1870b43c236a04d
Slope <matthewna1031@gmail.com>
2022-09-08 21:21:43 +0900

    Name acquisition more concise, check format

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
 mod cli;
 mod helper;
 
-use cli::*;
 use clap::Parser;
+use cli::*;
 
 fn main() {
     let cli = Cli::parse();
 
     let v = cli.verbose;
-    let name = match cli {
-        Cli {
-            output: Some(name),
-            ..
-        } => {
-                name
-            },
-        Cli {
-            file,
-            output: None,
-            ..
-        } => {
-                // WARNING will crash if file name containts invalid Unicode
-                file.file_name().unwrap().to_str().unwrap().to_string()
-            }
+    let output_name = match cli.output {
+        Some(name) => name,
+        // WARNING will crash if file name containts invalid Unicode
+        // TODO replace file extension
+        None => cli.file.file_name().unwrap().to_str().unwrap().to_string(),
     };
 
-    vprintln!(v, "File name: {}", name);
+    vprintln!(v, "File name: {}", output_name);
+
+    vprintln!(v, "Format: {:?}", cli.format);
+
+    match cli.format {
+        Format::Sh => {}
+        _ => {}
+    }
 }
