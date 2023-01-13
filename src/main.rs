ac3bb7255f544ae61ff8b864e8d1211767c03540
Slope <linearfunc@proton.me>
2023-01-13 15:23:35 +0900

    cleaning

--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,31 @@ mod helper;
 
 use clap::Parser;
 use cli::*;
-use format::*;
-use std::fs;
 use env::Env;
+use std::fs;
 use std::io::Write;
 use std::path::PathBuf;
+use std::process::ExitCode;
 
-fn main() {
+fn main() -> ExitCode {
     let cli = Cli::parse();
 
     // exit if input is not a file
     if !cli.file.is_file() {
         eprintln!("Input file is not a file.");
-        return;
+        return ExitCode::FAILURE;
     }
 
     // initialize relevant information
     let v = cli.verbose;
-    let output_name = match cli.output {
-        Some(name) => name,
-        None => format!(
-            "{}.{}",
-            match cli.file.file_stem() {
-                None => panic!("No file name!"),
-                // WARNING will crash if file name contains invalid Unicode, fix somehow idk
-                // does this even need fixing?
-                stem => stem.unwrap().to_str().unwrap().to_string(),
-            },
-            cli.format.to_string()
-        ),
+    let output_path = match cli.output {
+        Some(file_name) => PathBuf::from(file_name),
+        None => cli.file.with_extension(cli.format.to_string()),
     };
 
     // vprint useful information
     vprintln!(v, "Input File: {:?}", cli.file);
-    vprintln!(v, "Output File: {}", output_name);
+    vprintln!(v, "Output File: {:?}", output_path);
     vprintln!(v, "Format: {}", cli.format);
 
     // TODO do this safely, better error messages
@@ -51,9 +42,11 @@ fn main() {
     let output = data.to_string(cli.format);
 
     // create new output file, clear if already exists
-    let mut output_file = fs::File::create(output_name).unwrap();
+    let mut output_file = fs::File::create(output_path).unwrap();
 
     // write output to file
     // TODO do it more safely
     output_file.write_all(output.as_bytes()).unwrap();
+
+    ExitCode::SUCCESS
 }
