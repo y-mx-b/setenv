f0b3cf1f96b695944adb203b6ecdfa466d3bcdee
Slope <linearfunc@proton.me>
2023-01-13 15:23:35 +0900

    Added comments, verbose print

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,21 @@ use format::*;
 fn main() {
     let cli = Cli::parse();
 
-    let v = cli.verbose;
-
+    // exit if input is not a file
     if !cli.file.is_file() {
         eprintln!("Input file is not a file.");
         return;
     }
 
+    // initialize relevant information
+    let v = cli.verbose;
     let output_name = match cli.output {
         Some(name) => name,
         None => format!(
             "{}.{}",
             match cli.file.file_stem() {
                 None => panic!("No file name!"),
-                // WARNING will crash if file name containts invalid Unicode, fix somehow idk
+                // WARNING will crash if file name contains invalid Unicode, fix somehow idk
                 // does this even need fixing?
                 stem => stem.unwrap().to_str().unwrap().to_string(),
             },
@@ -30,10 +31,12 @@ fn main() {
         ),
     };
 
-    vprintln!(v, "File name: {}", output_name);
-
+    // vprint useful information
+    vprintln!(v, "Input File: {:?}", cli.file);
+    vprintln!(v, "Output File: {}", output_name);
     vprintln!(v, "Format: {}", cli.format);
 
+    // convert input TOML to shell script
     match cli.format {
         Format::Sh => {}
         _ => {}
